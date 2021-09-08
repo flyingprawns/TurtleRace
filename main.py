c9401dc2f86caf0f13b0e8c12f78124e5275fd53
Qijie Zhu <qzhu9@ucsc.edu>
2021-09-07 18:13:24 -0700

    created turtles and set their colors

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+
+# Create turtles and screen
+turtle_count = 6
+my_turtles = []
+for i in range(turtle_count):
+    my_turtles.append(Turtle(shape='turtle'))
 screen = Screen()
 
+# Set turtle colors
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+for i in range(turtle_count):
+    my_turtles[i].color(colors[i])
 
+# Setup screen
+screen.setup(width=500, height=400)
+
+# Get user to place a bet
+user_bet = screen.textinput(title='Make your bet!', prompt='Which turtle will win the race? Enter a color: ')
+print(user_bet)
+
+
+# Move some turtles
+
+
+
+# Exit program on user click
 screen.exitonclick()
+print('Turtle race is over. Hope you had fun!')
