8691a60579b16841eb6eb92014eb2898e4174acc
Qijie Zhu <qzhu9@ucsc.edu>
2021-09-07 19:52:52 -0700

    turtles set to starting positions

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 from turtle import Turtle, Screen
 
-
-# Create turtles and screen
+# Create turtles
 turtle_count = 6
 my_turtles = []
 for i in range(turtle_count):
     my_turtles.append(Turtle(shape='turtle'))
-screen = Screen()
 
 # Set turtle colors
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+colors = ['red', 'orange', 'gold', 'green', 'blue', 'purple']
 for i in range(turtle_count):
     my_turtles[i].color(colors[i])
 
 # Setup screen
+screen = Screen()
 screen.setup(width=500, height=400)
 
 # Get user to place a bet
 user_bet = screen.textinput(title='Make your bet!', prompt='Which turtle will win the race? Enter a color: ')
 print(user_bet)
 
-
-# Move some turtles
-
-
+# Move turtles to starting positions
+y_position = -135
+for turtle in my_turtles:
+    turtle.penup()
+    turtle.goto(-225, y_position)
+    turtle.pendown()
+    y_position += 55
 
 # Exit program on user click
 screen.exitonclick()
